f224b32a5408dc814fe1a002a04f07755c18cd0e
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2026-09-15 15:00:02 -0300

    Tests

---
+++ ZanzarProject/ZanzarProjectTests/ZanzarProjectTests.swift
@@ -0,0 +1,18 @@
+//
+//  ZanzarProjectTests.swift
+//  ZanzarProjectTests
+//
+//  Created by Lucas Dal Pra Brascher on 15/09/26.
+//
+
+import Testing
+
+struct ZanzarProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
